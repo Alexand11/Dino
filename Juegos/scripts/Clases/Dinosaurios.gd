b1eba1a89c903d51659923a289069a0c03000fb0
Jostin <127710192+Alexand11@users.noreply.github.com>
2024-03-14 02:08:44 +0100

    Add files via upload

--- Juegos/scripts/Clases/Dinosaurios.gd
+++ Juegos/scripts/Clases/Dinosaurios.gd
@@ -2,15 +2,40 @@ extends Node
 
 class_name Dinosaurios
 
-var life = 10
-var damage = 2
+var life_max = 500
+var life = 500
+var damage = 40
+var damage_crítico = 69
+var resistencia = 14
+var prob_Crítico = 15
+var energia_max = 20
+var energia = 1
 
 func Atacar(daño):
-	Signs.emit_signal("enviar_daño",damage)
+	if energia >= 2:
+		if Calcular_Crítico() == true:
+			daño = damage_crítico
+		Signs.emit_signal("enviar_daño",daño)
+		energia -= 2
 
 func Recibir_Daño(daño):
+	daño -= resistencia
 	life -= daño
-	print("me has quitado vida")
+	print("me causo :"+str(daño))
+
+func  Calcular_Crítico():
+	var num = randi_range(0,100)
+	print(num)
+	if num < prob_Crítico:
+		print("crítico :D "+ str(num))
+		return true
+	else:
+		print("no crítico :( " + str(num))
+		return false
+
 
 func Cambiar_Datos():
+	$Vida.max_value = life_max
 	$Vida.value = life
+	$Energia.max_value = energia_max
+	$Energia.value = energia
--- Juegos/scripts/player.gd
+++ Juegos/scripts/player.gd
@@ -6,3 +6,11 @@ func _ready():
 
 func _process(delta):
 	Cambiar_Datos()
+
+
+func _on_base_pressed():
+	Atacar(damage)
+
+
+func _on_terminar_turo_pressed():
+	energia += 1
